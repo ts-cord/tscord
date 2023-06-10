bfcc3c055fd5b9e62aa7b7d084a7d6c509f5b223
yUnreal <unrealwebdev@gmail.com>
2023-06-10 09:27:09 -0300

    Upper snake case change

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,5 +1,5 @@
 import { AxiosInstance, default as axios } from "axios";
-import { api as DiscordAPIURL } from "./constants.json";
+import { DISCORD_API_URL as DiscordAPIURL } from "./constants.json";
 
 /** Base for all requests */
 
